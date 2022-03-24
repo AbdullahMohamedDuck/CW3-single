b4d11280b11f713d40427f2e489af6e347a12d5e
Abdullah Mohamed <abdullahabdin@live.com>
2022-03-24 11:57:58 +0000

    image update

---
+++ webshop/src/components/productlist.vue
@@ -0,0 +1,125 @@
+<template>
+    <main>
+     <figure>
+        <img v-bind:src='product.image'>
+        </figure>
+        <h2>{{product.topic}}</h2>
+        <p>Lesson ID: {{product.productID}}</p>
+        <p>Description: {{product.description}}</p>
+        <p>Location: {{product.location}}</p>
+        <p>Price: £{{product.price}}</p>
+        <p>Spaces: {{product.Spaces }}</p>
+        <button @click='addItem(product)' v-bind:disabled='!canAddToCart(product)'><span
+                class="fas fa-shopping-basket"></span>Add to cart</button>   
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'products',
+    data(){
+        return {
+            products:[
+                {
+    "id":1001,
+    "title":"Subject: Maths",
+    "description":"Learn maths with this subject",
+    "price":99,
+    "location": "London",
+    "image":"images/maths.jpg", 
+    "availableInventory":5   
+                },
+                {  
+    "id":1002,
+    "title":"Subject: Art",
+    "description":"Get creative with Art classes",
+    "price": 100,
+    "location": "London",
+    "image":"images/art.jpg", 
+    "availableInventory":5     
+},
+
+{  
+    "id":1003,
+    "title":"Subject: Biology",
+    "description":"Discover the human body!",
+    "price": 150,
+    "location": "London",
+    "image":"images/biology.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1004,
+    "title":"Subject: Chemistry",
+    "description":"Learn chemistry ",
+    "price": 200,
+    "location": "Liverpool",
+    "image":"images/chemistry.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1005,
+    "title":"Subject: Computing",
+    "description":"Learn to code!",
+    "price":129,
+    "location": "York",
+    "image":"images/computing.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1006,
+    "title":"Subject: Engineering",
+    "description":"Learn the design!",
+    "price":200,
+    "location": "Manchester",
+    "image":"images/engineering.jpg", 
+    "availableInventory":5
+},
+{  
+    "id":1007,
+    "title":"Subject: English",
+    "description":"Learn English with this subject",
+    "price":99,
+    "location": "Cambridge",
+    "image":"images/english.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1008,
+    "title":"Subject: Gym",
+    "description":"Get fit with Gym classes!!",
+    "price":40,
+    "location": "London",
+    "image":"images/gym.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1009,
+    "title":"Subject: Music",
+    "description":"Learn to play instruments",
+    "price":99,
+    "location": "Sheffield",
+    "image":"images/music.jpg", 
+    "availableInventory":5     
+},
+{  
+    "id":1010,
+    "title":"Subject: Physics",
+    "description":"Learn Physics with this subject",
+    "price":200,
+    "location": "Bristol",
+    "image":"images/physics.jpg", 
+    "availableInventory":5     
+}
+            ]
+        }
+    },
+    methods: {
+        addItem(product) {
+        this.cart.push(product)
+        product.Spaces--
+    },
+    }
+}
+
+</script>
