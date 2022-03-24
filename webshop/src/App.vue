b5801b35e338af5f8c1c653197ef09af5ed84705
Abdullah Mohamed <abdullahabdin@live.com>
2022-03-24 12:52:12 +0000

    shows lesson checkpoint

--- webshop/src/App.vue
+++ webshop/src/App.vue
@@ -4,17 +4,16 @@
       <h1>{{sitename}}</h1>
       <button @click="showCheckout">{{this.cart.length}} Checkout</button>
     </header>
+    <product-list @addProduct='addToCart' > </product-list>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import productList from './components/productlist.vue'
 
 export default {
   name: 'App',
-  // components: {
-  //   HelloWorld
-  // },
+  components: { productList },
   data() {
     return {
       sitename: 'Vue.js Lesson Shop',
@@ -22,29 +21,24 @@ export default {
     }
   },
   methods: {
- showCheckout() {
-    this.showProduct = this.showProduct ? false : true;
-    if (this.showProduct == false) {
-    if (this.cart.length > 0) {
-
-    this.order.productID = this.cart[0].productID;
-    this.order.Spaces = this.cart[0].Spaces;
-    console.log("Order", this.order);
-      }
-    }
+  showCheckout() {},
                             
-   }
+  addToCart(product) {
+    this.cart.push(product)
+    // product.Spaces--
+   },
+  
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  <style>
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+  </style>
